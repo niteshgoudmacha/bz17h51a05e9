30b05dcbc35cbc42f268eea0366cc31b8a4774f5
NiteshNitu <niteshgoud7366@gmail.com>
2020-01-08 11:25:18 +0530

    changed server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist/bz17h51a05e9'));
+app.use(express.static(__dirname + '/dist/project1'));
 
 app.get('/', (req, res) => { 
-    res.sendFile(path.join(__dirname + '/dist/bz17h51a05e9/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/project1/index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
