c618368106d412c33be744641c42ae67b985e5d6
NiteshNitu <niteshgoud7366@gmail.com>
2020-01-08 11:17:03 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/dist/bz17h51a05e9'));
+
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname + '/dist/bz17h51a05e9/index.html'));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
